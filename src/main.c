0c329d10d437d9530bc9ecdbc979763ac27738f9
ameta <vildan.asckarov@gmail.com>
2021-04-20 19:49:24 +0300

    .

--- src/main.c
+++ src/main.c
@@ -6,33 +6,33 @@
 /*   By: ameta <ameta@student.21-school.ru>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/04/20 13:34:25 by ameta             #+#    #+#             */
-/*   Updated: 2021/04/20 19:30:11 by ameta            ###   ########.fr       */
+/*   Updated: 2021/04/20 19:47:20 by ameta            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/cub.h"
 
-int		error(char *message)
+int		ft_print_error(char *message)
 {
 	ft_putstr_fd("Error\n", 2);
 	ft_putstr_fd(message, 2);
 	exit(1);
 }
 
-static int	input_check(int ac, char **av, t_cub *cub)
+static int	ft_check_input(int ac, char **av, t_cub *cub)
 {
 	if (ac != 2 || ac != 3)
-		return (error("Error: invalid amount of arguments\n"));
-	else if (ft_strncmp(argv[1] + (ft_strlen(argv[1]) - 4), ".cub", 4))
-		return (error("Error: scene file should have a .cub extension\n"));
-	if (argc == 3)
+		return (ft_print_error("Invalid amount of arguments\n"));
+	else if (ft_strncmp(av[1] + (ft_strlen(av[1]) - 4), ".cub", 4))
+		return (error("File must have a '.cub' extension\n"));
+	if (ac == 3)
 	{
-		if (ft_strncmp(argv[2], "--save", 7))
-			return (error("Error: try --save as second argument\n"));
+		if (ft_strncmp(av[2], "--save", 7))
+			return (ft_print_error("Use '--save' as a second argument\n"));
 		else
-			scene->save_bmp = 1;
+			cub->save_bmp = 1;
 	}
-	return (SUCCESS);
+	return (1);
 }
 
 int			main(int ac, char **av)
@@ -40,23 +40,23 @@ int			main(int ac, char **av)
 	t_cub   cub;
 
 	ft_bzero(&cub, sizeof(t_cub));
-	input_check(ac, av, &cub);
-	scene.fd = open(argv[1], O_RDWR);
-	if (scene.fd == ERROR)
-		return (error("Error: .cub file could not be openend\n"));
+	ft_check_input(ac, av, &cub);
+	cub.fd = open(av[1], O_RDWR);
+	if (cub.fd == -1)
+		return (ft_print_error("File didn't open\n"));
 	else
-		scene.fd_flag = 1;
-	scene.ret = 1;
-	while (scene.ret > 0)
+		cub.fd_flag = 1;
+	cub.ret = 1;
+	while (cub.ret > 0)
 	{
-		scene.ret = get_next_line(scene.fd, &scene.line);
-		if (scene.ret == ERROR)
-			return (error("Error: .cub file could not been read\n"));
-		parse_scene(&scene, scene.line);
-		free(scene.line);
+		cub.ret = get_next_line(cub.fd, &cub.line);
+		if (cub.ret == -1)
+			return (ft_print_error("File reading failed\n"));
+		parse_scene(&cub, cub.line);
+		free(cub.line);
 	}
-	close(scene.fd);
-	scene_check(&scene);
-	start_game(&scene);
+	close(cub.fd);
+	scene_check(&cub);
+	start_game(&cub);
 	exit(0);
 }
